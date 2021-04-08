60bc53f5a081d82ac3e5b2f3dcae9ac3598a4254
Sowmya <sowmyagadam24@gmail.com>
2021-04-07 21:58:10 -0400

    fix: Connection issues fixed

--- cvat/settings/development.py
+++ cvat/settings/development.py
@@ -11,7 +11,9 @@ INSTALLED_APPS += [
     'django_extensions',
 ]
 
-ALLOWED_HOSTS.append('testserver')
+# ALLOWED_HOSTS.append('testserver')
+
+ALLOWED_HOSTS = ["*"]
 
 # Django-sendfile:
 # https://github.com/johnsensible/django-sendfile
@@ -30,9 +32,10 @@ DATABASES = {
 # Cross-Origin Resource Sharing settings for CVAT UI
 UI_SCHEME = os.environ.get('UI_SCHEME', 'http')
 UI_HOST = os.environ.get('UI_HOST', 'localhost')
-UI_PORT = os.environ.get('UI_PORT', 3000)
-CORS_ALLOW_CREDENTIALS = True
-CSRF_TRUSTED_ORIGINS = [UI_HOST]
+UI_PORT = os.environ.get('UI_PORT', 4200)
+# CORS_ALLOW_CREDENTIALS = True
+CORS_ORIGIN_ALLOW_ALL = True
+# CSRF_TRUSTED_ORIGINS = [UI_HOST]
 UI_URL = '{}://{}'.format(UI_SCHEME, UI_HOST)
 
 if UI_PORT and UI_PORT != '80':
@@ -40,5 +43,6 @@ if UI_PORT and UI_PORT != '80':
 # set UI url to redirect to after successful e-mail confirmation
 ACCOUNT_EMAIL_CONFIRMATION_ANONYMOUS_REDIRECT_URL = '{}/auth/login'.format(UI_URL)
 
-CORS_ORIGIN_WHITELIST = [UI_URL]
-CORS_REPLACE_HTTPS_REFERER = True
+#CORS_ORIGIN_WHITELIST = [UI_URL,
+#                         'http://localhost:4200']
+# CORS_REPLACE_HTTPS_REFERER = True
